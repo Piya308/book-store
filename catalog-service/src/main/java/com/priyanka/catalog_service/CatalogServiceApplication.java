41481802031bec4430538cc272e0b0eb021c0442
Piya308 <priyankagaikwad1185@gmail.com>
2026-02-22 22:35:35 +0530

    created configuration file for catalog service

--- catalog-service/src/main/java/com/priyanka/catalog_service/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/priyanka/catalog_service/CatalogServiceApplication.java
@@ -1,9 +1,15 @@
 package com.priyanka.catalog_service;
 
+import com.priyanka.catalog_service.domain.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
+//@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
