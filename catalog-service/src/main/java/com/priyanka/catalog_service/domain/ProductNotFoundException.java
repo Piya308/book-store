9da654d6295622a1d679f63039009a0b80ad5fda
Piya308 <priyankagaikwad1185@gmail.com>
2026-02-26 22:28:59 +0530

    added global exception

---
+++ catalog-service/src/main/java/com/priyanka/catalog_service/domain/ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package com.priyanka.catalog_service.domain;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String msg) {
+        super(msg);
+    }
+    public static ProductNotFoundException createWithCode(String code){
+        return new ProductNotFoundException("Product not found with given code:-"+code);
+    }
+}
---
+++ catalog-service/src/main/java/com/priyanka/catalog_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.priyanka.catalog_service.exception;
+
+import com.priyanka.catalog_service.domain.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/not-found");
+    private static final URI ISE_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/server-error");
+    private static final String SERVICE_NAME = "catalog-service";
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    ProblemDetail  handleProductNotFoundException(ProductNotFoundException e){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Product not found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", LocalDateTime.now());
+        return problemDetail;
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail  handleGenericUnhandledException(Exception e){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+}
