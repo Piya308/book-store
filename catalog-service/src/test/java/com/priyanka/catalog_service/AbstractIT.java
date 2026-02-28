133b96320e825de073a4004d2eb23f5d401013e4
Piya308 <priyankagaikwad1185@gmail.com>
2026-02-28 17:27:18 +0530

    reverting code if does notwork

--- catalog-service/src/test/java/com/priyanka/catalog_service/AbstractIT.java
+++ catalog-service/src/test/java/com/priyanka/catalog_service/AbstractIT.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
-//@Import(TestcontainersConfiguration.class)
+@Import(TestCatalogServiceApplication.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIT {
     @LocalServerPort
