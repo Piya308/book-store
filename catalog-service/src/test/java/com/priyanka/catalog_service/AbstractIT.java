1c4d195df0fe0e3adaf5b8fbd7acfc82d556b816
Piya308 <priyankagaikwad1185@gmail.com>
2026-03-03 22:57:19 +0530

    Moved test files in respective folders

--- catalog-service/src/test/java/com/priyanka/catalog_service/AbstractIT.java
+++ catalog-service/src/test/java/com/priyanka/catalog_service/AbstractIT.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
 public abstract class AbstractIT {
     @LocalServerPort
     int port;
