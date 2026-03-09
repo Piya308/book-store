902a5f22bafd58cd38491a0265b85fa86ee23bf8
Piya308 <priyankagaikwad1185@gmail.com>
2026-03-09 23:49:50 +0530

    create rabbitmq config

---
+++ order-service/src/main/java/com/priyanka/order_service/config/RabbitMQConfig.java
@@ -0,0 +1,84 @@
+package com.priyanka.order_service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.priyanka.order_service.domain.ApplicationProperties;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+
+@Configuration
+public class RabbitMQConfig {
+    private final ApplicationProperties applicationProperties;
+
+    public RabbitMQConfig(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Bean
+    DirectExchange exchange(){
+        return new DirectExchange(applicationProperties.orderEventsExchange());
+    }
+
+    @Bean
+    Queue newOrdersQueue(){
+        return QueueBuilder.durable(applicationProperties.newOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding newOrdersQueueBinding(){
+        return BindingBuilder.bind(newOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.newOrdersQueue());
+    }
+    @Bean
+    Queue deliveredOrdersQueue(){
+        return QueueBuilder.durable(applicationProperties.deliveredOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding deliverdOrdersQueueBinding(){
+        return BindingBuilder.bind(deliveredOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.deliveredOrdersQueue());
+    }
+
+    @Bean
+    Queue cancelledOrdersQueue(){
+        return QueueBuilder.durable(applicationProperties.cancelledOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding cancelledOrdersQueueBinding(){
+        return BindingBuilder.bind(cancelledOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.cancelledOrdersQueue());
+    }
+
+    @Bean
+    Queue errorOrdersQueue(){
+        return QueueBuilder.durable(applicationProperties.cancelledOrdersQueue()).build();
+    }
+
+    @Bean
+    Binding errorOrdersQueueBinding(){
+        return BindingBuilder.bind(errorOrdersQueue())
+                .to(exchange())
+                .with(applicationProperties.errorOrdersQueue());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, ObjectMapper objectMapper){
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter(objectMapper));
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(ObjectMapper objectMapper){
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
